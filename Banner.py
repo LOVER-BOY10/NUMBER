5af7e7122f09a37210cf084db7a1cf40569e10ce
mfk lihan34 <118593996+LOVER-BOY10@users.noreply.github.com>
2025-12-26 12:41:30 +0600

    Add files via upload

---
+++ Banner.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import time
+os.system("clear")
+print("\n\n\n")
+Banner="			System Loading...."
+for c in Banner:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.2)
+
+print("\n\n\n")
+os.system("clear")
+time.sleep(0.2)
+print("\n\n\n")
+Banner="			Successfully Loaded\n"
+for c in Banner:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.2)
+Name=input("			Enter Your Name:")
+Banner=("			Hay "+Name+",be Ethikal Hack")
+for c in Banner:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.2)
+print("\n")
+os.system("clear")
+time.sleep(2)	
+print("""
+    __  __ ______ _  __  _      _____ _    _          _   _  
+ |  \\/  |  ____| |/ / | |    |_   _| |  | |   /\\   | \\ | | 
+ | \\  / | |__  | ' /  | |      | | | |__| |  /  \\  |  \\| | 
+ | |\\/| |  __| |  <   | |      | | |  __  | / /\\ \\ | . ` | 
+ | |  | | |    | . \\  | |____ _| |_| |  | |/ ____ \\| |\  | 
+ |_|  |_|_|    |_|\\_\\ |______|_____|_|  |_/_/    \\_\\_| \\_| 
+===============================                                                           \033[1;40mOwner : MFK LIHAN\033[0m
+\033[1;36mGitHub : BANGLADESH\033[0m
+\033[1;31mFacebook : FAZLUL KARIM\033[0m
+\033[1;35mNumber : 01610052155\033[0m
+\033[1;34m===========================\033[0m""")	
